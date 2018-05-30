ee870438e280b6563f204b6ed206d23a7aef7dfd
chris <s1116195@student.windesheim.nl>
2018-05-30 11:17:57 +0200

    toe voegen lampjes verbetering

--- src/ASRSController/BPPArduino.java
+++ src/ASRSController/BPPArduino.java
@@ -11,12 +11,15 @@ class BPPArduino extends Arduino {
     void sendOrder(List<Package> packages, List<Container> containers) {
         byte[] commands = new byte[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 99};
         int current = 0;
+
+/*
         List<Package> lastPackageContainers = new ArrayList<Package>();
         for(Container c : containers){
             if(c.getPackages().size() == 0){ continue; }
             int tempLength = c.getPackages().size()-1;
             lastPackageContainers.add(c.getPackages().get(tempLength));
         }
+*/
 
         for (Package p : packages) {
             if (containers.get(0).getPackages().contains(p)) {
@@ -24,6 +27,7 @@ class BPPArduino extends Arduino {
             } else {
                 commands[current] = 98;
             }
+/*
             if(lastPackageContainers.contains(p)){
                 current++;
                 if(containers.get(0).getPackages().contains(p)){
@@ -32,7 +36,25 @@ class BPPArduino extends Arduino {
                     commands[current] = 102;
                 }
             }
+*/
             current++;
+        }
+
+        boolean leftFull = false;
+        boolean rightFull = false;
+        for (int i = commands.length; i >= 0;i--) {
+            if(commands[i] == 97){
+                if(!leftFull){
+                    commands[i+1] = 101;
+                    leftFull = true;
+                }
+            }
+            if(commands[i] == 98){
+                if(!rightFull){
+                    commands[i+1] = 102;
+                    rightFull = true;
+                }
+            }
         }
 
         try {
